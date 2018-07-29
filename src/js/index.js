814ef85a799e55de674834db5fe981f08036afd8
skyroxas <skyroxas1521@gmail.com>
2018-07-29 15:25:21 +0800

    refactor: 引入 aos 的 scroll 套件

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
-import './import-jquery';
-import 'bootstrap';
-import './slideshow';
+import './import-jquery'
+import 'bootstrap'
+import './slideshow'
+
+import AOS from 'aos'
+// import 'aos/dist/aos.css'
+// GitHub: https://github.com/michalsnik/aos
+
+// AOS.init()
+
+// document.onLoad = () => {
+//   AOS.init()
+//
+// }
+//
+window.addEventListener('load', () => {
+    AOS.init()
+})
